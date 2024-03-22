9199f671db6562eaeee7c544550e3917f632e51e
whosmars <mariosalazar@ciencias.unam.mx>
2024-03-22 09:56:03 -0600

    Comments 2

--- script.sh
+++ script.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-# Asegúrate de proporcionar el PID correcto del proceso del navegador como argumento
+# Make sure to provide the correct PID of the browser process as an argument
 PID=$1
 
-# Verifica que se haya proporcionado un PID
+
+# Check if the PID have been provided
 if [ -z "$PID" ]; then
   echo "Uso: $0 <PID del navegador>"
   exit 1
 fi
 
-# Directorio donde buscar descriptores de archivo
+# Relative path to the file descriptor directory
 fd_dir="/proc/$PID/fd"
 
-# Verifica que el directorio exista (es decir, que el PID sea válido)
+# Check if the directory exists (i.e., the PID is valid)
 if [ ! -d "$fd_dir" ]; then
   echo "El PID proporcionado no corresponde a un proceso existente."
   continue
@@ -21,18 +22,18 @@ fi
 
 for fd in $fd_dir/*; do
   fd_number=$(basename "$fd")
-
-  # Ignora descriptores de archivo especiales
+  # 0: stdin, 1: stdout, 2: stderr
+  # Ignore special file descriptors
   if [[ "$fd_number" == "0" || "$fd_number" == "1" || "$fd_number" == "2" ]]; then
     continue
   fi
 
-  # Comprueba si el descriptor de archivo todavía existe antes de intentar leerlo
+  # Check if the file descriptor still exists before trying to read it
   if [ ! -e "$fd" ]; then
-    continue  # Salta al siguiente descriptor si este ya no existe
+    continue  # Continue to the next file descriptor if it no longer exists
   fi
 
-  # Buscar URLs
+  # Find URLs in the file descriptor using grep
  timeout 2s  grep -Ea 'http://[a-zA-Z0-9.-]*/' "$fd"  2>/dev/null
 done
 
